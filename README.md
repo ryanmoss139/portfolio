a7cfc2ebda5af5068429a24472e2560abd43d3a4
Ryan Moss <Ryanmoss139@btinternet.com>
2021-06-04 16:17:50 +0100

    New updates to the calculator have been made. These changes can be found in the README doc. Simple readbility changes and imported the time function to include sleep throughout for better flow of the program.

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ Python 3
 To start off, I have created a simple calculator with simple functionality (add, subtract, divide and multiply). It takes user input and stores each number in a seperate variable. Following this, the user is prompted for an option, [a], [s], [d] or [m] with each letter corresponding to the beginning letter of the mathematical operation they wish to use. The input is then converted into lower case. Each number is passed into the corresponding function that we created to perform our mathematical operation. This will return the value and present it to the user. We use an if statement to run through the conditions of the users option and select the relevant function to return the correct result to the user. 
 
 This is a very simple calculator and I plan to add further functionality and potentially a user interface to this later down the line using tkinter. However, I created this as a demonstration of simple readable code, good coding practices and a calculator is always useful to have! 
+
+Update 1:
+I have decided to contain the conditional statement inside of a function just to clean up the code a little more. 
+
+Update 2:
+To add a little better flow to the program for the user, I imported time so I could use sleep() throughout my program. It breaks up the input a little and the result from the end message so the user can see there calculation clearly. I also cleaned up the code a little more as I noticed a few things I could do to improve readability. 
+
+I plan to later to include tkinter into this project. I have previously learned it, but I would like to become a little more fluent with it as I didnt spend much time with it previously. However, I currently have my hands tied down with Chat Bots and Natural Language Proecessing. I also plan to have another shell calculator similar to this which will interact with the user and give human-like responses, loop through the program if the user wishes and close out the program with simple words like 'goodbye' or 'close'
--- calc.py
+++ calc.py
@@ -1,13 +1,16 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 
 """ Calculator Notes
 
-Create add() function
-Create subtract() function
-Create divide() function
-Create multiply() function
+Now we have simple functionality, we need to think about how we can improve this 
+
+Import time and use time.sleep() to improve general flow of program for user 
+
+Scan through code and double check it is all readable, noted and clean
 
 """
+
+import time
 
 # First we introduce any variables we might require later on in the program
 # Including a little welcome message of course
@@ -22,6 +25,7 @@ print(welcome_message)
 # Take input from the user to store the numbers they want to use
 num1 = int(input("Enter your first number: "))
 num2 = int(input("Enter your second number: "))
+time.sleep(1)
 
 # Create our options to present to the user
 option1 = "[a] Add \n"
@@ -30,45 +34,56 @@ option3 = "[d] Divide \n"
 option4 = "[m] Multiply \n"
 
 # Present options to the user and take a selection as input
-option = input("Please select an option:\n{option1}{option2}{option3}{option4} \n> ".format(option1=option1, option2=option2, option3=option3, option4=option4))
-option.lower()
+option = input(f"\nPlease select an option:\n{option1}{option2}{option3}{option4} \n> ")
+option = option.lower()
 
-# Create our add(self) function 
+print("\nProcessing Calculation...")
+time.sleep(2)
+
+
+# Create our add() function 
 def add(num1, num2):
 	return num1 + num2 
 
-# Create our add(self) function 
+# Create our add() function 
 def subtract(num1, num2):
 	return num1 - num2 
 
-# Create our add(self) function 
+# Create our add() function 
 def divide(num1, num2):
 	return num1 / num2 
 
-# Create our add(self) function 
+# Create our add() function 
 def multiply(num1, num2):
 	return num1 * num2 
 
+def check_sum(num1, num2):
+	# Our conditional arguments to test the option input from the user and provide a result
+	if option == 'a':
+		add_result = add(num1, num2)
+		print(f"\nResult: {num1} + {num2} = {add_result}")
+	elif option == 's':
+		subtract_result = subtract(num1, num2)
+		print(f"\nResult: {num1} - {num2} = {subtract_result}")
+	elif option == 'd':
+		divide_result = divide(num1, num2)
+		print(f"\nResult: {num1} / {num2} = {divide_result}")
+	elif option =='m':
+		multiply_result = multiply(num1, num2)
+		print(f"\nResult: {num1} x {num2} = {multiply_result}")
+	else:
+		# Error message incase the user pressed an incorrect option
+		print("Sorry, please try again using a valid option from the menu.")
 
+# Run check function to check the numbers and the option and produce result
+check_sum(num1, num2)
 
-if option == 'a':
-	add_result = add(num1, num2)
-	print("\n {num1} + {num2} = {add_result}".format(num1=num1, num2=num2, add_result=add_result))
-elif option == 's':
-	subtract_result = subtract(num1, num2)
-	print("\n {num1} - {num2} = {subtract_result}".format(num1=num1, num2=num2, subtract_result=subtract_result))
-elif option == 'd':
-	divide_result = divide(num1, num2)
-	print("\n {num1} / {num2} = {divide_result}".format(num1=num1, num2=num2, divide_result=divide_result))
-elif option =='m':
-	multiply_result = multiply(num1, num2)
-	print("\n \n > >  {num1} x {num2} = {multiply_result}".format(num1=num1, num2=num2, multiply_result=multiply_result))
-else:
-	print("Sorry, please try again using a valid option from the menu.")
-
+time.sleep(2)
+# A nice output before the program closes 
 print("""
-	Thanks for using my Calculator :) 
+Thanks for using my Calculator :) 
 
-	Calculator coded by: mossydem
-	GitHub: github.com/ryanmoss139
-	""")
+Calculator coded by: Ryan Moss
+GitHub: github.com/ryanmoss139
+""")
+time.sleep(2)
